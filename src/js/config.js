e22ef44b467f074c6a90854317885acd9c27be7c
KhizarSa <khizarsaeed80@gmail.com>
2021-09-18 12:31:50 +0500

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dfb56c55-6799-4def-aef8-4b86b7b681d9';
+export const MODEL_CLOSE_SEC = 2.5;
